e3d73bfffdf89b4585b11b266106b5d3f9b2b160
Bastian Waidelich <bastian@typo3.org>
2013-02-08 15:55:20 +0100

    [TASK] Add HAL Browser resources

---
+++ Classes/Wwwision/Hal/Controller/BrowserController.php
@@ -0,0 +1,27 @@
+<?php
+namespace Wwwision\Hal\Controller;
+
+/*                                                                        *
+ * This script belongs to the Flow package "Wwwision.Hal".                *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ */
+class BrowserController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * @return void
+	 */
+	public function indexAction() {
+	}
+
+}
+?>
